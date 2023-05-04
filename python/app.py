cd4c0a89ffea6f5ebedd824ec64832c6f25aaa8e
ZsambokiDavid <david.zsamboki@schrack-seconet.hu>
2023-05-04 11:28:33 +0000

    dunno

---
+++ python/app.py
@@ -0,0 +1,22 @@
+import mariadb
+import sys
+
+def main():
+
+    try:
+        conn = mariadb.connect(
+            user="david",
+            password="admin",
+            host="mariadb",
+            port=3306,
+            database="mariadb"
+
+         )
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
+
+    cur = conn.cursor()
+
+if __name__ == "__main__":
+    main()
