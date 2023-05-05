4a143478a4a03d412b2e4b07d30f732dc08d20ab
ZsambokiDavid <david.zsamboki@schrack-seconet.hu>
2023-05-05 11:01:51 +0000

    -y

--- python/requirements.sh
+++ python/requirements.sh
@@ -2,7 +2,7 @@
 apt-get -y update
 apt install python3-pip
 pip install --upgrade pip
-pip install -y wheel
+pip -y install wheel
 apt install wget
 wget https://r.mariadb.com/downloads/mariadb_repo_setup
 echo "ad125f01bada12a1ba2f9986a21c59d2cccbe8d584e7f55079ecbeb7f43a4da4  mariadb_repo_setup" \
@@ -10,6 +10,6 @@ echo "ad125f01bada12a1ba2f9986a21c59d2cccbe8d584e7f55079ecbeb7f43a4da4  mariadb_
 chmod +x mariadb_repo_setup
 ./mariadb_repo_setup \
    --mariadb-server-version="mariadb-10.6"
-apt install -y libmariadb3 libmariadb-dev
-python3 -m pip install -y mariadb --no-cache-dir
+apt -y install libmariadb3 libmariadb-dev
+python3 -m pip -y install mariadb #--no-cache-dir
 #python -m pip download --only-binary :all: --dest . --no-cache mariadb
