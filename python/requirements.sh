1c566b4c30749efbc95f21306a59242e99b7f337
ZsambokiDavid <david.zsamboki@schrack-seconet.hu>
2023-05-05 10:49:31 +0000

    dwnl

--- python/requirements.sh
+++ python/requirements.sh
@@ -12,4 +12,4 @@ chmod +x mariadb_repo_setup
    --mariadb-server-version="mariadb-10.6"
 apt install libmariadb3 libmariadb-dev
 #python3 -m pip install mariadb --no-cache-dir
-python -m pip install --only-binary :all: --dest . --no-cache mariadb
+python -m pip download --only-binary :all: --dest . --no-cache mariadb
