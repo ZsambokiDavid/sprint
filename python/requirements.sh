06b1515a2f51be3d648d5141535bdbd571889201
ZsambokiDavid <david.zsamboki@schrack-seconet.hu>
2023-05-05 11:04:15 +0000

    relocate

--- python/requirements.sh
+++ python/requirements.sh
@@ -2,7 +2,7 @@
 apt-get -y update
 apt install python3-pip
 pip install --upgrade pip
-pip -y install wheel
+python3 -y pip install wheel
 apt install wget
 wget https://r.mariadb.com/downloads/mariadb_repo_setup
 echo "ad125f01bada12a1ba2f9986a21c59d2cccbe8d584e7f55079ecbeb7f43a4da4  mariadb_repo_setup" \
@@ -11,5 +11,5 @@ chmod +x mariadb_repo_setup
 ./mariadb_repo_setup \
    --mariadb-server-version="mariadb-10.6"
 apt -y install libmariadb3 libmariadb-dev
-python3 -m pip -y install mariadb #--no-cache-dir
+python3 -m -y pip install mariadb #--no-cache-dir
 #python -m pip download --only-binary :all: --dest . --no-cache mariadb
