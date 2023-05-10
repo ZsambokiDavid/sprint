07f3ebb3cdbd6fe03402a6a87f2f3d4bfe6163a7
ZsambokiDavid <david.zsamboki@schrack-seconet.hu>
2023-05-10 12:01:35 +0000

    -y

--- python/requirements.sh
+++ python/requirements.sh
@@ -3,7 +3,7 @@ apt-get -y update
 apt install python3-pip
 pip install --upgrade pip
 pip install wheel
-apt install wget
+apt -y install wget
 wget https://r.mariadb.com/downloads/mariadb_repo_setup
 echo "ad125f01bada12a1ba2f9986a21c59d2cccbe8d584e7f55079ecbeb7f43a4da4  mariadb_repo_setup" \
     | sha256sum -c -
